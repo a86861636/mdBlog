2e2e0cfebb59f36e7b7d236415e1e9443ca38324
a86861636 <1612956183@qq.com>
2021-12-08 17:44:20 +0800

    替换文章图片路径

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
-//node.js读取项目文件目录
+'use strict'
 const fs = require("fs")
 try {
-  console.log('读取目录中')
+  //读取项目文件目录
+  console.log('读取文章目录中')
   let menuArr = []
-  fs.readdirSync('./').forEach(file => {
+  let articleDir = './article/'
+  fs.readdirSync(articleDir).forEach(file => {
     if (file.indexOf('.') == -1) {
       let obj = {
         title: file,
         articles: []
       }
-      fs.readdirSync('./' + file).forEach(file1 => {
+      fs.readdirSync(articleDir + file).forEach(file1 => {
         if (file1.indexOf('.md') > -1) {
           obj.articles.push(file1.replace('.md', ''))
         }
@@ -17,9 +19,27 @@ try {
       menuArr.push(obj)
     }
   })
-  // console.log(arr)
-  console.log('修改README.md')
-  const githubUrl = 'https://github.com/a86861636/mdBlog/tree/master'
+
+  //替换article文件夹里的md文件中的图片路径
+  console.log('修改图片路径')
+  const imageUrl = 'https://raw.githubusercontent.com/a86861636/mdBlog/main/image'
+  const imageKey = '../../image'
+  for (let item of menuArr) {
+    item.articles.forEach(art => {
+      console.log(art)
+      const artPath = articleDir + item.title + '/' + art + '.md'
+      const artFile = fs.readFileSync(artPath)
+      const artFileStr = artFile.toString()
+      if (artFileStr.indexOf('../../image') > -1) {
+        let replaceStr = artFileStr.replace(RegExp('../../image', 'g'), imageUrl)
+        fs.writeFileSync(artPath, replaceStr)
+      }
+    })
+  }
+
+  //修改README.md中文章目录
+  console.log('修改README.md中')
+  const githubUrl = 'https://github.com/a86861636/mdBlog/tree/main'
   const mdFile = fs.readFileSync('./README.md')
   const mdFileStr = mdFile.toString()
   let oldMenuStr = mdFileStr.match(`<!-- 目录 start -->([\\s\\S]*?)<!-- 目录 end -->`)[1]
@@ -33,6 +53,7 @@ try {
   }
   fs.writeFileSync('./README.md', mdFileStr.replace(oldMenuStr, newMenuStr))
   console.log('修改完成')
+
 } catch (e) {
   console.log(e)
   console.log('失败')
